657eac5a7fc24844ed8b32e2f4beda3c1aaeae9e
DemonicFlame <arkaprovo007@gmail.com>
2024-07-07 00:33:50 +0530

    added front-end functionality

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -3,8 +3,40 @@ import { dbank } from "../../declarations/dbank";
 window.addEventListener("load", async () => {
   try {
     const currentAmount = await dbank.checkBalance();
-    document.getElementById("value").innerText = currentAmount;
+    document.getElementById("value").innerText =
+      Math.round(currentAmount * 100) / 100;
   } catch (error) {
-    console.error("Error fetching balance:", error);
+    console.error(error);
+  }
+});
+
+document.querySelector("form").addEventListener("submit", async (event) => {
+  try {
+    event.preventDefault();
+    const button = event.target.querySelector("#submit-btn");
+
+    const inputAmount = parseFloat(
+      document.getElementById("input-amount").value
+    );
+    const outputAmount = parseFloat(
+      document.getElementById("withdrawal-amount").value
+    );
+    button.setAttribute("disabled", true);
+    if (document.getElementById("input-amount").value.length != 0) {
+      await dbank.topUp(inputAmount);
+    }
+    if (document.getElementById("withdrawal-amount").value.length != 0) {
+      await dbank.withdraw(outputAmount);
+    }
+    await dbank.compound();
+
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerText =
+      Math.round(currentAmount * 100) / 100;
+    document.getElementById("input-amount").value = "";
+    document.getElementById("withdrawal-amount").value = "";
+    button.removeAttribute("disabled");
+  } catch (error) {
+    console.log(error);
   }
 });
